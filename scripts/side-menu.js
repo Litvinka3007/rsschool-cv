5c5ef93f7b0dfe52de377f197f9f79cb915f06a0
Angelina <angelina16161616@gmail.com>
2021-09-17 22:24:20 +0300

    feat: add script for the mobile navigation

---
+++ scripts/side-menu.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function openSlideMenu() {
+  document.getElementById('side-menu').style.width = '250px';
+}
+function closeSlideMenu() {
+  document.getElementById('side-menu').style.width = '0';
+}
